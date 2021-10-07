16a79507ed0fb714309a28b872e299b27d49c972
JayoonKoo <rnwkdbs12@gmail.com>
2021-10-07 18:34:15 +0900

    feat: Handle disconnection

--- socket.js
+++ socket.js
@@ -1,6 +1,8 @@
 module.exports = (io) => {
 	io.on('connection', (socket) => {
 		console.log('A User is now connected');
+		socket.on('disconnect', () => {
+			console.log('disconnected');
+		});
 	});
-	
 }
