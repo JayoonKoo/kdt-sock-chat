018f7b903928189a20daa4e19f08016a316f483f
JayoonKoo <rnwkdbs12@gmail.com>
2021-10-07 16:36:42 +0900

    feat: Add IndexRouter
    app.js 에서 라우터 분리

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const app = express();
-
 const http = require('http');
 const server = http.createServer(app);
 
-app.get('/', (req, res) => {
-	res.send('Express works!')
-})
+const indexRouter = require('./routes');
+
+app.use('/', indexRouter);
 
 server.listen(3000, () => {
 	console.log("Express Server is now listening on localhost:3000...");
