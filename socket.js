c0ff0fcec08d1ecf62d0f445a27a27243f363d86
JayoonKoo <rnwkdbs12@gmail.com>
2021-10-07 18:52:54 +0900

    feat: Broadcasting message (socketio done)

--- socket.js
+++ socket.js
@@ -2,6 +2,8 @@ module.exports = (io) => {
 	io.on('connection', (socket) => {
 		console.log('A User is now connected');
 		socket.on('chat message', (msg) => {
+			// 브로드 케스팅 서버에 연결된 모든 사용자에게 뿌리기 ... 
+			io.emit('chat message', msg);
 			console.log(`message :`, msg);
 		})
 		socket.on('disconnect', () => {
