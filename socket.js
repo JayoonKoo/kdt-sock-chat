87df4a98859d409314d2e1f7c35dc7d013091a7d
JayoonKoo <rnwkdbs12@gmail.com>
2021-10-07 18:43:08 +0900

    feat: Chat console log

--- socket.js
+++ socket.js
@@ -1,6 +1,9 @@
 module.exports = (io) => {
 	io.on('connection', (socket) => {
 		console.log('A User is now connected');
+		socket.on('chat message', (msg) => {
+			console.log(`message :`, msg);
+		})
 		socket.on('disconnect', () => {
 			console.log('disconnected');
 		});
